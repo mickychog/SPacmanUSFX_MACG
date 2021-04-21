19f8c51a076add697849310d25b9012b10a64f13
mickychog <mickychog@gmail.com>
2021-04-21 12:20:53 -0400

    metodos accesores

--- PPacmanUSFX_MACG/Pacman.h
+++ PPacmanUSFX_MACG/Pacman.h
@@ -9,5 +9,16 @@ private:
 	float velocidadY;
 public:
 	void move(float _velocidadX, float _velocidadY);
+	void move(float _velocidadX, float _velocidadY) {};
+
+	float getPosicionX() { return posicionX; }
+	void setPosicionX(float _posicionX) { posicionX = _posicionX; }
+	float getPosicionY() { return posicionY; }
+	void setPosicionY(float _posicionY) { posicionY = _posicionY; }
+
+	float getVelocidadX() { return velocidadX; }
+	void setVelocidadX(float _velocidadX) { velocidadX = _velocidadX; }
+	float getVelocidadY() { return velocidadY; }
+	void setVelocidadY(float _velocidadY) { velocidadY = _velocidadY; }
 };
 
--- PPacmanUSFX_MACG/main_basico.cpp
+++ PPacmanUSFX_MACG/main_basico.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "Pacman.h"
 using namespace std ;
 int main() {
+	Pacman pacmancito;
 	cout << "main de prueba" << endl;
 	return 0;
 }
